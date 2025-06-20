e1fc7221b031aa9dff38e8319ec49466fb7b804d
justin lee <justin.hisung.lee@gmail.com>
2025-06-20 19:03:58 -0400

    finished get_token() func

--- to_mp3.py
+++ to_mp3.py
@@ -1,5 +1,6 @@
 # spotify API
 # take a spotify playlist and grab every song from the playlist, append it to spotify_songs
+import json
 import webbrowser
 import requests
 import base64
@@ -15,51 +16,30 @@ client_secret = os.getenv("CLIENT_SECRET")
 print(f"client_id: {client_id}")
 print(f"client_secret: {client_secret}")
 
-auth_headers = {
-    "client_id": client_id,
-    "response_type": "code",
-    "redirect_uri": "http://127.0.0.1:8000/callback",
-    "scope": "user-library-read"
-}
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
-webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
+    url = "https://accounts.spotify.com/api/token"
 
-auth_code = os.getenv("AUTH_CODE")
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
 
-print(f"auth_code: {auth_code}")
+    data = {"grant_type": "client_credentials"}
+    result = requests.post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
 
-encoded_credentials = base64.b64encode(client_id.encode() + b':' + client_secret.encode()).decode("utf-8")
+token = get_token()
 
-token_headers = {
-    "Authorization": "Basic " + encoded_credentials,
-    "Content-Type": "application/x-www-form-urlencoded"
-}
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
 
-token_data = {
-    "grant_type": "authorization_code",
-    "code": auth_code,
-    "redirect_uri": "http://127.0.0.1:8000/callback"
-}
-
-r = requests.post("https://accounts.spotify.com/api/token", data=token_data, headers=token_headers)
-
-token = r.json()
-print("==")
 print(token)
-print("==")
-
-user_headers = {
-    "Authorization": "Bearer " + token,
-    "Content-Type": "application/json"
-}
-
-user_params = {
-    "limit": 50
-}
-
-user_tracks_response = requests.get("https://api.spotify.com/v1/me/tracks", params=user_params, headers=user_headers)
-
-print(user_tracks_response.json())
 
 # youtube API?
 # take every song on the playlist and look it up on youtube, append it to yt_links (LIST)

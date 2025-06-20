ffb52e7644ebde1513d55495c41ceb46fe58956b
justin lee <justin.hisung.lee@gmail.com>
2025-06-19 21:40:42 -0400

    added .gitignore file

---
+++ to_mp3.py
@@ -0,0 +1,17 @@
+# spotify API
+# take a spotify playlist and grab every song from the playlist, append it to spotify_songs
+
+import requests
+
+
+
+# youtube API?
+# take every song on the playlist and look it up on youtube, append it to yt_links (LIST)
+# (need to determine how to choose which one)
+
+
+
+
+
+# youtube -> spotify API
+# convert song to mp3 and download it
